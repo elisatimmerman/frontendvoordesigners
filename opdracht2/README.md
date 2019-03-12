bdf473cbdc28fd66e5794ba2110c50ff9756962d
Elisa Timmerman <elisa_timmerman@quicknet.nl>
2019-03-12 21:38:21 +0100

    Opdracht DEF

--- opdracht2/README.md
+++ opdracht2/README.md
@@ -4,8 +4,13 @@
 Vandaag heb ik een begin gemaakt met de opdracht. Het is wat laat omdat ik het heel druk had met het project Interaction.
 
 Verloop.
-Het zoeken naar een goede carousel is best moeilijk. Ik heb op internet gekeken naar voorbeelden. Dit waren voornamelijk hele moeilijke code waar ik niks van begreep. Daarna heb ik zelf een beetje nagedacht en ging ik daarna weer op zoek naar een voorbeeld code op internet. Deze kwam ik tegen. Het was code zonder javascript en het werkt prima. Om nog een beetje javascript te schrijven heb ik korte omschrijvingen bij de afbeeldingen gedaan.
+Het zoeken naar een goede carousel is best moeilijk. Ik heb op internet gekeken naar voorbeelden. Dit waren voornamelijk hele moeilijke code waar ik niks van begreep. Daarna heb ik zelf een beetje nagedacht en ging ik daarna weer op zoek naar een voorbeeld code op internet. Deze kwam ik het tegen. Het was code zonder javascript en het werkt prima. Om nog een beetje javascript te schrijven heb ik korte omschrijvingen bij de afbeeldingen gedaan.
 
 Ik wil met de website nog de puntjes op de i. Daar ga ik morgen aan werken.
+
+12 maart 2019
+Overdag had ik zelf de code geschreven en aangepast. Het resultaat was wat ik wilde bereiken maar ik wist ook dat de javascript beter kon. Daarom had ik in de avond met mijn broertje samen naar de javascript gekeken. Samen hebben wij er voor gezorgd dat de afbeelding in een array kwam en dat alles in twee function kwam.
+
+Link: https://elisatimmerman.github.io/frontendvoordesigners/opdracht2/
 
 
--- opdracht2/script/script.js
+++ opdracht2/script/script.js
@@ -2,93 +2,106 @@
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
 
+
+
+var srcAfbeeldingen = [{
+        klasse: "slide-staand",
+        id: "slide-1",
+        src: "images/1.jpg",
+        alt: "kerk"
+    },
+    {
+        klasse: "slide-staand",
+        id: "slide-2",
+        src: "images/2.jpg",
+        alt: "begraafplaats"
+    },
+    {
+        klasse: "slide-liggend",
+        id: "slide-3",
+        src: "images/3.jpg",
+        alt: "gebouw"
+    },
+    {
+        klasse: "slide-staand",
+        id: "slide-4",
+        src: "images/4.jpg",
+        alt: "grasveld"
+    },
+    {
+        klasse: "slide-staand",
+        id: "slide-5",
+        src: "images/5.jpg",
+        alt: "museum"
+    }];
+
+
+/*Hiermee zorg je ervoor dat alle afbeeldingen die in de array srcAfbeeldingen staan in de div met class='slider' komen*/
+function laadAfbeeldingen() {
+    var html = '';
+    var i;
+    for (i = 0; i < srcAfbeeldingen.length; i++) {
+        html += '<img class=' + srcAfbeeldingen[i].klasse + ' id=' + srcAfbeeldingen[i].id + ' src=' + srcAfbeeldingen[i].src + ' alt=' + srcAfbeeldingen[i].alt + '>';
+    }
+
+    document.querySelector(".slider").innerHTML += html;
+}
+
+laadAfbeeldingen();
+
 var afbeeldingGrootEen = document.getElementById('slide-1');
 var afbeeldingGrootTwee = document.getElementById('slide-2');
 var afbeeldingGrootDrie = document.getElementById('slide-3');
 var afbeeldingGrootVier = document.getElementById('slide-4');
 var afbeeldingGrootVijf = document.getElementById('slide-5');
 
+var groteAfbeeldingen = [afbeeldingGrootEen, afbeeldingGrootTwee, afbeeldingGrootDrie, afbeeldingGrootVier, afbeeldingGrootVijf];
 
 
 
-function tekstEen(event) {
-    event.preventDefault();
-    document.getElementById('beschrijving').textContent = 'In Dublin heb je heel veel kerken. Dit is er een van. Hier kwamen wij elke dag langs omdat het vlakbij onze hostel lag. Het lag boven op een helling waardoor je het van ver kon zien.';
+/*De eerste loop reset de styling die eventueel gezet kan zijn. De swtich zorgt ervoor dat het juiste element wordt getoond. De laatste loop zorgt ervoor dat alle andere elementen een brightness van 40% hebben*/
 
-    afbeeldingGrootEen.style.filter = 'brightness(100%)';
-    afbeeldingGrootTwee.style.filter = 'brightness(40%)';
-    afbeeldingGrootDrie.style.filter = 'brightness(40%)';
-    afbeeldingGrootVier.style.filter = 'brightness(40%)';
-    afbeeldingGrootVijf.style.filter = 'brightness(40%)';
+function tekst(nummer) {
+    var i;
+
+    for (i = 0; i < groteAfbeeldingen.length; i++){
+        groteAfbeeldingen[i].style.filter = null;
+    }
+
+    switch (nummer) {
+        case 1:
+            groteAfbeeldingen[0].style.filter = 'brightness(100%)';
+            document.getElementById('beschrijving').textContent = 'In Dublin heb je heel veel kerken. Dit is er een van. Hier kwamen wij elke dag langs omdat het vlakbij onze hostel lag. Het lag boven op een helling waardoor je het van ver kon zien.';
+            break;
+        case 2:
+            groteAfbeeldingen[1].style.filter = 'brightness(100%)';
+            document.getElementById('beschrijving').textContent = 'Hier zie je een gedeelte van de grootste begraafplaats van Dublin. We hadden busrit waar we ook konden uitstappen. Dit was hiervan een gedeelte. Het was heel erg groot en heel erg indrukwekkend. Er stonden ook graven van ongeveer een eeuw oud. Hoe Bizar!';
+            break;
+        case 3:
+            groteAfbeeldingen[2].style.filter = 'brightness(100%)';
+            document.getElementById('beschrijving').textContent = 'Dit is een groot gebouw. Ik weet niet wat het precies is maar dit kwamen we ook tegen tijdens de busrit.';
+            break;
+        case 4:
+            groteAfbeeldingen[3].style.filter = 'brightness(100%)';
+            document.getElementById('beschrijving').textContent = 'Deze foto is ook genomen toen we bezig waren met de busrit. We stapten ergens uit en gingen toen chillen. De paal die je ziet is volgens mij een monument. Het was heerlijk om hier te zitten. Het was zonnig en er stond weinig wind.';
+            break;
+        case 5:
+            groteAfbeeldingen[4].style.filter = 'brightness(100%)';
+            document.getElementById('beschrijving').textContent = 'Dit was een winkeltje waar je allemaal Guinness (bier uit Dublin) kan kopen. Daarna kregen we een hele leuke rondleiding. En natuurlijk niet te vergeten een biertje!';
+            break;
+    }
+
+    for (i = 0; i < groteAfbeeldingen.length; i++){
+        if(groteAfbeeldingen[i].style.filter !== 'brightness(100%)') {
+            groteAfbeeldingen[i].style.filter = 'brightness(40%)';
+        }
+    }
 }
 
-document.getElementById('afbeeldingEen').addEventListener('click', tekstEen, false);
 
-
-
-
-
-function tekstTwee(event) {
-    event.preventDefault();
-    document.getElementById('beschrijving').textContent = 'Hier zie je een gedeelte van de grootste begraafplaats van Dublin. We hadden busrit waar we ook konden uitstappen. Dit was hiervan een gedeelte. Het was heel erg groot en heel erg indrukwekkend. Er stonden ook graven van ongeveer een eeuw oud. Hoe Bizar!';
-
-    afbeeldingGrootEen.style.filter = 'brightness(40%)';
-    afbeeldingGrootTwee.style.filter = 'brightness(100%)';
-    afbeeldingGrootDrie.style.filter = 'brightness(40%)';
-    afbeeldingGrootVier.style.filter = 'brightness(40%)';
-    afbeeldingGrootVijf.style.filter = 'brightness(40%)';
-}
-
-document.getElementById('afbeeldingTwee').addEventListener('click', tekstTwee, false);
-
-
-
-
-function tekstDrie(event) {
-    event.preventDefault();
-    document.getElementById('beschrijving').textContent = 'Dit is een groot gebouw. Ik weet niet wat het precies is maar dit kwamen we ook tegen tijdens de busrit.';
-
-    afbeeldingGrootEen.style.filter = 'brightness(40%)';
-    afbeeldingGrootTwee.style.filter = 'brightness(40%)';
-    afbeeldingGrootDrie.style.filter = 'brightness(100%)';
-    afbeeldingGrootVier.style.filter = 'brightness(40%)';
-    afbeeldingGrootVijf.style.filter = 'brightness(40%)';
-}
-
-document.getElementById('afbeeldingDrie').addEventListener('click', tekstDrie, false);
-
-
-
-
-function tekstVier(event) {
-    event.preventDefault();
-    document.getElementById('beschrijving').textContent = 'Deze foto is ook genomen toen we bezig waren met de busrit. We stapten ergens uit en gingen toen chillen. De paal die je ziet is volgens mij een monument. Het was heerlijk om hier te zitten. Het was zonnig en er stond weinig wind.';
-    afbeeldingGrootEen.style.filter = 'brightness(40%)';
-    afbeeldingGrootTwee.style.filter = 'brightness(40%)';
-    afbeeldingGrootDrie.style.filter = 'brightness(40%)';
-    afbeeldingGrootVier.style.filter = 'brightness(100%)';
-    afbeeldingGrootVijf.style.filter = 'brightness(40%)';
-}
-
-document.getElementById('afbeeldingVier').addEventListener('click', tekstVier, false);
-
-
-
-
-function tekstVijf(event) {
-    event.preventDefault();
-    document.getElementById('beschrijving').textContent = 'Dit was een winkeltje waar je allemaal Quinness (bier uit Dublin) kan kopen. Daarna kregen we een hele leuke rondleiding. En natuurlijk niet te vergeten een biertje!';
-    afbeeldingGrootEen.style.filter = 'brightness(40%)';
-    afbeeldingGrootTwee.style.filter = 'brightness(40%)';
-    afbeeldingGrootDrie.style.filter = 'brightness(40%)';
-    afbeeldingGrootVier.style.filter = 'brightness(40%)';
-    afbeeldingGrootVijf.style.filter = 'brightness(100%)';
-}
-
-document.getElementById('afbeeldingVijf').addEventListener('click', tekstVijf, false);
-
-
-
-
-
-
+/*Aan de volgende elementen wordt een evenListener op klik gezet, deze roept tekst aan met als parameter de index van het element*/
+document.getElementById('afbeeldingEen').addEventListener('click', function() {tekst(1);}, false);
+document.getElementById('afbeeldingTwee').addEventListener('click', function() {tekst(2);}, false);
+document.getElementById('afbeeldingDrie').addEventListener('click', function() {tekst(3);}, false);
+document.getElementById('afbeeldingVier').addEventListener('click', function() {tekst(4);}, false);
+document.getElementById('afbeeldingVijf').addEventListener('click', function() {tekst(5);}, false);
