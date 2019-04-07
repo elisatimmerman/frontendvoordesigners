e730c3274be477ee550aa807fe2ba7231bd32385
Elisa Timmerman <elisa_timmerman@quicknet.nl>
2019-04-07 20:26:21 +0200

    Voorpagina en extra pagina gemaakt

--- opdracht3/Elisa/script/script.js
+++ opdracht3/Elisa/script/script.js
@@ -2,54 +2,55 @@
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
 
-var jsonUrl = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json'; //json file op github
-var button = document.querySelector('button');
+/*const betekent dat je hem niet mag aanpassen */
+
+const jsonUrl = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json'; //json file op github
+const button = document.querySelector('button');
 // var evilDeadPage = window.location="../evildead/evildead.html";
 // var evilDeadPage = document.getElementById('1');
-var loaderElement = document.querySelector('span');
-var section = document.querySelector('section');
+const loaderElement = document.querySelector('span');
+const section = document.querySelector('section');
 // console.log("loaderElement",loaderElement);
 
-function showMovie(movie) {
-  console.log('hoi');
-  
-}
-
 function showData(movies) {
-  for (var i = 0; i < movies.length; i++) {
-    var movie = movies[i];
-    var movieContainer = document.createElement('article');
+  for (let i = 0; i < movies.length; i++) {
+    const movie = movies[i];
+    const movieContainer = document.createElement('article');
+    movieContainer.id = movie.id;
 
-    movieContainer.onclick = showMovie(movie);
-
-    var movieCoverElement = document.createElement('img');
+    const movieCoverElement = document.createElement('img');
     movieCoverElement.src = movie.cover;
     movieContainer.appendChild(movieCoverElement);
 
-    var releaseDateElement = document.createElement('p');
+    const releaseDateElement = document.createElement('p');
     releaseDateElement.textContent = movie.release_date;
     movieContainer.appendChild(releaseDateElement);
 
-    var movieTitleElement = document.createElement('h2');
+    const genreElement = document.createElement('p');
+    genreElement.textContent = movie.genres;
+    movieContainer.appendChild(genreElement);
+
+    const movieTitleElement = document.createElement('h2');
     movieTitleElement.textContent = movie.title;
     movieContainer.appendChild(movieTitleElement);
 
-    var simplePlotElement = document.createElement('p');
+    const simplePlotElement = document.createElement('p');
     simplePlotElement.textContent = 'Description: ' + movie.simple_plot;
     movieContainer.appendChild(simplePlotElement);
 
-    // var evilDeadId = movies[i].id;
-    // var evilDead = window.location.href="../evildead/evildead.html";
+    const buttonElement = document.createElement('button');
+    buttonElement.innerHTML = 'Meer informatie';
+    movieContainer.appendChild(buttonElement);
 
-    // evilDeadId.onclick = function () {
-    //   evilDead.classList.add('show');
-    // }
+    section.appendChild(movieContainer);
 
-    section.appendChild(movieContainer)
+    document.getElementById(movie.id).addEventListener('click', () => {
+      showMovie(movie);
+    });
   }
 }
 
-function loadMoviesFromJson(){
+function loadMoviesFromJson() {
   fetch(jsonUrl).then((response) => {
     response.json().then((movies) => {
       showData(movies);
@@ -58,9 +59,46 @@ function loadMoviesFromJson(){
 }
 loadMoviesFromJson();
 
-// button.onclick = function (event) {
-//   event.preventDefault();
-//   var evilDeadPage = document.getElementById('1');
-//   // window.location = "../evildead/evildead.html";
-// }
+function showMovie(movie) {
+  console.log(movie);
+  const movieInfo = document.createElement('article');
+
+  const movieCoverElement = document.createElement('img');
+  movieCoverElement.src = movie.cover;
+  movieInfo.appendChild(movieCoverElement);
+
+  const releaseDateElement = document.createElement('p');
+  releaseDateElement.textContent = movie.release_date;
+  movieInfo.appendChild(releaseDateElement);
+
+  const movieTitleElement = document.createElement('h2');
+  movieTitleElement.textContent = movie.title;
+  movieInfo.appendChild(movieTitleElement);
+
+  const plotElement = document.createElement('p');
+  plotElement.textContent = 'Description: ' + movie.plot;
+  movieInfo.appendChild(plotElement);
+
+  for (let i = 0; i < movie.actors.length; i++) {
+    const ActorElement = generateActorElement (movie.actors[i]);
+    movieInfo.appendChild(ActorElement);
+  }
+
+  while(section.firstChild){
+    section.removeChild(section.firstChild);
+  }
+  
+  section.appendChild(movieInfo);
+}
+
+function generateActorElement (actor) {
+  const actorContainer = document.createElement('article')
+
+  const actorNameElement = document.createElement('h3');
+  actorNameElement.textContent = actor.actor_name;
+  actorContainer .appendChild(actorNameElement);
+  /*Opmaka voor 1 actor, return actorElement*/
+  return actorContainer;
+}
+
 
